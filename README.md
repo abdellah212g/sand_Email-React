593397def9e9e85632c575c169d2f2c5b70bdf84
abdoumar <abdoumar@gmail.com>
2021-04-29 11:59:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sand_Email-React
+ abdellah212g/sand_Email-React
---
+++ server/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config({ path: './config/.env' });
+// packages
+const express = require('express');
+// const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const contactRoutes = require('./routes/contact.routes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+app.use(cors());
+// if (process.env.NODE_ENV === 'development') app.use(morgan('tiny'));
+// Db Connexion
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Mongo Db Connected'))
+  .catch((err) => console.log(`error connection to the DataBase : ${err}`));
+// Routes
+app.use('/api/contact', contactRoutes);
+
+// app express
+app.listen(PORT, () => {
+  console.log(`app listning : localhost:${PORT}`);
+});
